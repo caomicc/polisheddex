40fd512b9a4b180b56141f2e0b2cca680c6bf8af
Cameron Omiccioli <caomicc@gmail.com>
2025-07-10 12:15:47 -0400

    feat: Integrate Vercel Analytics for enhanced tracking and performance insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Navigation } from "@/components/ui";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -84,6 +84,7 @@ export default function RootLayout({
             {children}
           </main>
         </div>
+        <Analytics />
       </body>
     </html>
   );
