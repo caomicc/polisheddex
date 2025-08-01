53aa30f44c803c917e158f603e3bf52a0726d100
Cameron Omiccioli <caomicc@gmail.com>
2025-08-01 14:58:27 -0400

    refactor: Simplify card components and replace CardAction with Button for improved UI consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Hero } from '@/components/ui/Hero';
 import Link from 'next/link';
 
@@ -21,57 +15,63 @@ export default function Home() {
         <div>
           <div className="flex flex-col sm:flex-row sm:space-x-12 space-y-4">
             <div className="flex flex-col">
-              <h2 className="text-xl mb-4 font-bold">Links:</h2>
               <div className="flex flex-col space-y-6">
                 <Link href="/pokemon" className="group">
-                  <Card className="gap-2 py-4 md:py-6">
-                    <CardHeader className="px-4 md:px-6">
-                      <CardTitle>Pokémon Database</CardTitle>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>
+                        <h3>Pokémon Database</h3>
+                      </CardTitle>
                     </CardHeader>
-                    <CardContent className="text-sm px-4 md:px-6">
-                      <p>
-                        {' '}
+                    <CardContent>
+                      <p className="text-sm">
                         Browse all Pokémon, their types, evolutions, moves, and where to find them.
                       </p>
                     </CardContent>
-                    <CardFooter className="text-sm px-4 md:px-6">
-                      <CardAction className="text-blue-600 font-medium group-hover:underline">
-                        View Pokémon &rarr;
-                      </CardAction>
+                    <CardFooter>
+                      <Button variant={'default'} size={'sm'}>
+                        View Pokémon
+                      </Button>
                     </CardFooter>
                   </Card>
                 </Link>
 
                 <Link href="/locations" className="group">
-                  <Card className="gap-2 py-4 md:py-6 dark:bg-white/5">
-                    <CardHeader className="px-4 md:px-6">
-                      <CardTitle>Location Guide</CardTitle>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>
+                        <h3>Location Guide</h3>
+                      </CardTitle>
                     </CardHeader>
-                    <CardContent className="text-sm px-4 md:px-6">
-                      <p>
+                    <CardContent>
+                      <p className="text-sm">
                         Explore all areas in the game and discover which Pokémon you can find in
                         each location.
                       </p>
                     </CardContent>
-                    <CardFooter className="text-sm px-4 md:px-6">
-                      <CardAction className="text-blue-600 font-medium group-hover:underline">
-                        View Locations &rarr;
-                      </CardAction>
+                    <CardFooter>
+                      <Button variant={'default'} size={'sm'}>
+                        View Locations
+                      </Button>
                     </CardFooter>
                   </Card>
                 </Link>
                 <Link href="/items" className="group">
-                  <Card className="gap-2 py-4 md:py-6 dark:bg-white/5">
-                    <CardHeader className="px-4 md:px-6">
-                      <CardTitle>Item Guide</CardTitle>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>
+                        <h3>Item Guide</h3>
+                      </CardTitle>
                     </CardHeader>
-                    <CardContent className="text-sm px-4 md:px-6">
-                      <p>Explore all items in the game and discover their effects and locations.</p>
+                    <CardContent>
+                      <p className="text-sm">
+                        Explore all items in the game and discover their effects and locations.
+                      </p>
                     </CardContent>
-                    <CardFooter className="text-sm px-4 md:px-6">
-                      <CardAction className="text-blue-600 font-medium group-hover:underline">
-                        View Items &rarr;
-                      </CardAction>
+                    <CardFooter>
+                      <Button variant={'default'} size={'sm'}>
+                        View Items
+                      </Button>
                     </CardFooter>
                   </Card>
                 </Link>
@@ -84,76 +84,34 @@ export default function Home() {
                   <div>
                     <h3>Polished vs Faithful checks</h3>
                     <p className="text-foreground/80 text-sm">
-                      Still handling some differences between the two versions, such as movesets and
-                      naming conventions.
+                      Still some bugs in my extractor in terms of parsing data for the different
+                      variations of the game.
                     </p>
-                    <span className="block text-muted-foreground text-xs mt-1 ml-1">
-                      At this point I have spent more time on this than actually playing the game. I
-                      built this because I wanted a guide to help me play the game, and I am still
-                      working on it to make it better for myself and others.
-                    </span>
-                    <span className="block text-muted-foreground font-medium text-xs mt-1 ml-1">
-                      I appreciate the patience as I work through the differences between the
-                      faithful vs polished versions of the game. I do understand that the
-                      perspective of this not being ready for use because of the differences not
-                      beging fully documented but I am only one person and I am trying very hard!
-                    </span>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-green-100 text-green-700 dark:bg-green-900 dark:text-green-200">
-                      In Progress
-                    </span>
                   </div>
                 </li>
                 <li className="flex items-start gap-4">
                   <div>
                     <h3>Item Database</h3>
                     <p className="text-foreground/80 text-sm">
-                      Browse all items, their effects, and where to find them.
+                      Basic item data is available, but not formatted.
                     </p>
-
-                    <span className="block text-muted-foreground text-xs mt-1 ml-1">
-                      *Basic item data is available, but not formatted.
-                    </span>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-green-100 text-green-700 dark:bg-green-900 dark:text-green-200">
-                      In Progress
-                    </span>
                   </div>
                 </li>
                 <li className="flex items-start gap-4">
                   <div>
                     <h3>Better Location Handling</h3>
                     <p className="text-foreground/80 text-sm">
-                      Add details to locations, such as items found, Pokémon available, move tutors,
-                      and more.
+                      Missing move tutors, this also includes making sure event pokemon and
+                      evolutions have a value displaying in locations tab on dex.
                     </p>
-                    <span className="block text-muted-foreground text-xs mt-1 ml-1">
-                      *Currently only basic location data is available + not all locations are
-                      included (cough, Pokemon found through headbutt)
-                    </span>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-green-100 text-green-700 dark:bg-green-900 dark:text-green-200">
-                      In Progress
-                    </span>
                   </div>
                 </li>
                 <li className="flex items-start gap-4">
                   <div>
                     <h3>Move Database</h3>
                     <p className="text-foreground/80 text-sm">
-                      Browse all moves, their effects, and type advantages vs disadvantages.
+                      Pages that will show what Pokemon can learn a specific move.
                     </p>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-yellow-100 text-yellow-700 dark:bg-yellow-900 dark:text-yellow-200">
-                      Planned
-                    </span>
-                  </div>
-                </li>
-                <li className="flex items-start gap-4">
-                  <div>
-                    <h3>Update UI</h3>
-                    <p className="text-foreground/80 text-sm">
-                      Improve UI for better usability and aesthetics while being mobile focused.
-                    </p>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-yellow-100 text-yellow-700 dark:bg-yellow-900 dark:text-yellow-200">
-                      Ongoing
-                    </span>
                   </div>
                 </li>
                 <li className="flex items-start gap-4">
@@ -163,23 +121,6 @@ export default function Home() {
                       A tool to help you build a team based on type match-ups, weaknesses, and
                       resistances for Pokemon and types specific to Polished Crystal.
                     </p>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-yellow-100 text-yellow-700 dark:bg-yellow-900 dark:text-yellow-200">
-                      Planned
-                    </span>
-                  </div>
-                </li>
-                <li className="flex items-start gap-4">
-                  <div>
-                    <h3>Game Mechanics Guide</h3>
-                    <p className="text-foreground/80 text-sm">
-                      A guide to the game mechanics, including battle mechanics, breeding, and more.
-                    </p>
-                    <span className="inline-block mt-2 px-2 py-0.5 text-xs rounded bg-gray-100 text-gray-700 dark:bg-gray-900 dark:text-gray-200">
-                      Idea
-                    </span>
-                    <span className="block text-muted-foreground text-xs mt-1 ml-1">
-                      *This may just default to wiki information instead of living on site, TBD
-                    </span>
                   </div>
                 </li>
               </ol>
